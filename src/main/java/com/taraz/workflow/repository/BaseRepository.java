124c67e79aab75583b1f0b163c7d090e23ed9bef
104913641 <Bizh@n!8830183>
2024-11-13 17:20:48 +0330

    config project

---
+++ src/main/java/com/taraz/workflow/repository/BaseRepository.java
@@ -0,0 +1,4 @@
+package com.taraz.workflow.repository;
+
+public class BaseRepository {
+}
---
+++ src/main/java/com/taraz/workflow/service/BpmnService.java
@@ -0,0 +1,7 @@
+package com.taraz.workflow.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BpmnService {
+}
