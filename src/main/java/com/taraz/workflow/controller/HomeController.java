5c97ca91ace7dc4d7b4f47ea49d8f4916cad86ed
104913641 <Bizh@n!8830183>
2024-11-13 17:24:27 +0330

    config project

--- src/main/java/com/taraz/workflow/controller/HomeController.java
+++ src/main/java/com/taraz/workflow/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.taraz.workflow.controller;
 
 
+import com.taraz.workflow.service.BpmnService;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.runtime.ProcessInstantiationBuilder;
@@ -11,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    private final BpmnService bpmnService;
+
+    public HomeController(BpmnService bpmnService) {
+        this.bpmnService = bpmnService;
+    }
 
     @GetMapping("/execute")
     public ResponseEntity<?> execute() {
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-        ProcessInstantiationBuilder processBuilder = processEngine.getRuntimeService().createProcessInstanceByKey("first_bpmn_id");
+        ProcessInstantiationBuilder processBuilder = bpmnService.createProcessInstantiation("first_bpmn_id");
         String item = "computer";
         processBuilder.setVariable("itemName", item);
         processBuilder.businessKey("execute-endpoint");
